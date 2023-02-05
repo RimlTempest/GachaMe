322b149321ccdf6a7aa416f18480caec1baeffe4
RimlTempest <riml.slime@gmail.com>
2023-02-06 03:27:27 +0900

    add: svgrを導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{html,js,jsx,ts,tsx}"],
-  safelist: [
-    "alert-info",
-    "alert-success",
-    "alert-error",
-    "alert-warning",
-    "alert-error",
-  ],
-  darkMode: "media",
+  content: ['./app/**/*.{html,js,jsx,ts,tsx}'],
+  safelist: ['alert-info', 'alert-success', 'alert-error', 'alert-warning', 'alert-error'],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
   variants: {},
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: 'winter',
+        mytheme: {
+          primary: '#66CC8A',
+        },
+      },
+    ],
+  },
 };
